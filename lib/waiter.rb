3f376cf3eac340b23e0a45f7619f009848cc0111
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-09-01 01:28:16 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -27,7 +27,7 @@ class Waiter
   end
   
   def best_tipper
-    meals_sorted = meals.sort_by {|meal| meal.tip}.
+    meals_sorted = meals.sort_by {|meal| meal.tip}.pop
     best_tipped_meal = meals.
   end
 
